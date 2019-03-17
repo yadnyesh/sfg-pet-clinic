800f2a05d10d5ca762576f7ecc030abc602b44ea
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-17 16:17:00 +0530

    feat: Read Properties for External source files Section 5: Lecture 94

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgPetClinicApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SfgPetClinicApplication.class, args);
+        
+        FakeDataSource fakeDataSource = (FakeDataSource) applicationContext.getBean(FakeDataSource.class);
+    
+        System.out.println(fakeDataSource.getUsername());
     }
 }
