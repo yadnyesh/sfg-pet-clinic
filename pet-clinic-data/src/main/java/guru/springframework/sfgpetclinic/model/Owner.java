607a17c6472790a7855ab2618f3063e7a8a3d956
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-26 13:31:11 +0530

    fix: i18n  Section 7: lecture 127

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -6,7 +6,35 @@ import java.util.Set;
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
+
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pet;
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getTelephone() {
+		return telephone;
+	}
+	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 	
 	public Set<Pet> getPet() {
 		return pet;
