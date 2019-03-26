8f3252f1eadfe8ae827bdd943512c62b080351d9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-03-26 10:38:38 +0530

    fix: i18n  Section 7: lecture 122

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/owner")
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 	
