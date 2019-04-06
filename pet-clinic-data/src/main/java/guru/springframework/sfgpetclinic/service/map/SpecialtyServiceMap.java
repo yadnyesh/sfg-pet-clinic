bd2dd337b0e1bda66cf4ab787698bc9d6aa7dba7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-04-06 11:16:49 +0530

    feat: Enhance SpecialtyServiceMap Section 7: lecture 131

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialtyServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialtyServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic.service.map;
 
 import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.service.SpecialtyService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 	
 	@Override
@@ -29,6 +31,6 @@ public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> imp
 	
 	@Override
 	public Specialty findById(Long id) {
-		super.findById(id);
+		return super.findById(id);
 	}
 }
