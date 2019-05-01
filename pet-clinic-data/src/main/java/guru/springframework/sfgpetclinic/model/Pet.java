337ec471074acd16cf01c596701b840a9dcef848
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-05-01 21:45:46 +0530

    feat: Section 8: lecture 155 - Added Visit JPA Entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by jt on 7/13/18.
@@ -24,7 +26,18 @@ public class Pet extends BaseEntity{
     
     @Column(name = "birth_date")
     private LocalDate birthDate;
-
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+    
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+    
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
+    
     public PetType getPetType() {
         return petType;
     }
