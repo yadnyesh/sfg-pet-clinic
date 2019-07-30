ffb5d80f30b1b95bfd4cccbd2557dff41bbf3363
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-07-30 09:16:41 +0530

    OwnerJPAService to demonstrate JPA implementation

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/jpa/OwnerJPAService.java
@@ -0,0 +1,84 @@
+package guru.springframework.sfgpetclinic.service.jpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repository.OwnerRepository;
+import guru.springframework.sfgpetclinic.repository.PetRepository;
+import guru.springframework.sfgpetclinic.repository.PetTypeRepository;
+import guru.springframework.sfgpetclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class OwnerJPAService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerJPAService(OwnerRepository ownerRepository, PetRepository petRepository,
+                           PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Owner object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
