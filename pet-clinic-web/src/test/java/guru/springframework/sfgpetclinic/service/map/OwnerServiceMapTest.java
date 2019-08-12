56393e3c1a37318004fdf140b5a44fdab5c8def5
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-08-12 09:51:12 +0530

    Important 191: JUnit 5 test begin

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/service/map/OwnerServiceMapTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerServiceMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
